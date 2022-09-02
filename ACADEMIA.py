b81c87082176141cab2d769b89de31bc50f79067
Matheus Melo <84096363+matheusmelo1294@users.noreply.github.com>
2022-09-02 09:47:03 -0300

    Add files via upload

---
+++ ACADEMIA.py
@@ -0,0 +1,135 @@
+print("****ACADEMIA FIT****")
+
+print("\n")
+
+name = input("NOME: ")
+
+print("\n")
+print(name, 'Olá!! Vamos malhar e ficar grande!!!')
+
+print("\n")
+
+print("1 - HOMEM")
+print("2 - MULHER")
+
+print('\n')
+
+n = 0
+while n == 0:
+  genre = int(input("DIGITE O NÚMERO DO SEU GÊNERO: "))
+
+  if(genre == 1):
+    print("Escolha o que você vai malhar")
+    print("\n")
+    print("1 - PEITO")
+    print("2 - OMBRO")
+    print("3 - TRÍCEPS")
+    print("4 - COSTAS")
+    print("5 - BÍCEPS")
+    print("6 - TRAPÉZIO")
+    print("7 - INFERIORES")
+    n = 0
+    while n == 0:
+      muscle1 = int(input("ESCOLHA O QUE VAI MALHAR: "))
+      if(muscle1 == 1):
+        print("\n")
+        print("***PEITO***")
+        print("\n")
+        print("SUPINO RETO - 3X10")
+        print("SUPINO INCLINADO - 4X12")
+        print("PECK DECK - 3X10")
+        print("CROSSOVER 4X10")
+        print("\n")
+      elif(muscle1 == 2):
+        print("\n")
+        print("***OMBRO***")
+        print("\n")
+        print("ELEVAÇÃO DE OMBROS - 3X12")
+        print("ELEVAÇÃO FRONTAL DE OMBROS - 3X10")
+        print("ELEVAÇÃO ARNOLD - 4X12")
+        print("\n")
+      elif(muscle1 == 3):
+        print("\n")
+        print("***TRÍCEPS***")
+        print("\n")
+        print("TRÍCEPS NA CORDA - 3X10")
+        print("TRÍCEPS NA POLIA - 3X12")
+        print('MERGULHO - 4X10')
+        print('\n')
+      elif(muscle1 == 4):
+        print("\n")
+        print("***COSTAS***")
+        print('\n')
+        print("BARRA FIXA 3X10")
+        print("REMADA CURVADA 3X10")
+        print("PULLOVER 4X12")
+        print("\n")
+      elif(muscle1 == 5):
+        print('\n')
+        print("***BÍCEPS***")
+        print("ROSCA DIRETA 3X10")
+        print("ROSCA INCLINADA 3X12")
+        print("ROSCA NA POLIA 4X10")
+        print("\n")
+      elif(muscle1 == 6):
+        print("\n")
+        print("REMADA VERTICAL 3X10")
+        print("ELEVAÇÃO DE OMBROS 4X12")
+        print('\n')
+      elif(muscle1 == 7):
+        print("\n")
+        print("AGACHAMENTO ISOMÉTRICO 3X10")
+        print("AGACHAMENTO BÚLGARO 3X12")
+        print("AVANÇO 3X10")
+        print("AGACHAMENTO LIVRE 3X10")
+        print("PANTURRILHA EM PÉ 3X12")
+        print("PANTURRILHA EM PÉ 5X10")
+        print('\n')
+      else:
+        print("Opção incorreta!!")
+
+  elif(genre == 2):
+    print("1 - TREINO A")
+    print("2 - TREINO B")
+    print("3 - TREINO C")
+    n = 0
+    while n == 0:
+      muscle2 = int(input("DIGITE O NÚMERO DO SEU TREINO: "))
+      if(muscle2 == 1):
+        print("TREINO A")
+        print("\n")
+        print("AGACHAMENTO 3X10")
+        print("EXTENSORA 4X12")
+        print("AGACHAMENTO SUMÔ 3X10")
+        print("CADEIRA ADUTORA 3X10")
+        print("ABDUÇÃO PRANCHA 3X12")
+        print("\n")
+      elif(muscle2 == 2):
+        print("TREINO B")
+        print("\n")
+        print("FLEXORA DEITADA 4X10")
+        print("LEG 45° 3X10")
+        print("STIFF 4X10")
+        print("ELEVAÇÃO UNILATERAL 3X10")
+        print("GLÚTEO GRAVITON 3X12")
+        print("PANTURRILHA EM PÉ 5X10")
+        print("PANTURRILHA SENTADA 4X12")
+        print("\n")
+      elif(muscle2 == 3):
+        print("TREINO C")
+        print("\n")
+        print("SUPINO RETO 3X10")
+        print("REMADA UNILATERAL 3X12")
+        print("PUXADOR FRENTE 3X10")
+        print("DESENVOLVIMENTO DE OMBROS 4X12")
+        print("ROSCA DIRETA COM HALTERES 3X10")
+        print("TRÍCEPS NA POLIA 4X10")
+      
+      
+      
+    
+
+  
+        
+      
+      
